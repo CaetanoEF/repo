73c679881a320a724fc2d669e252f6f8964bbdcf
CaetanoEF <163072811+CaetanoEF@users.noreply.github.com>
2024-08-08 21:54:41 -0300

    Teste

--- menu.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    menu.classList.toggle('showing');
-  });
